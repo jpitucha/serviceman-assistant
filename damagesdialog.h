8c01b2541631d60f2a7dbaa8c51b57659c59c3fa
obsceniczny <mr.cisco@protonmail.com>
2019-04-21 18:38:16 +0200

    Added slots, data and model in DamagesDialog.

--- damagesdialog.h
+++ damagesdialog.h
@@ -2,6 +2,7 @@
 #define DAMAGESDIALOG_H
 
 #include <QDialog>
+#include <QStringListModel>
 
 namespace Ui {
 class DamagesDialog;
@@ -13,10 +14,17 @@ class DamagesDialog : public QDialog
 
 public:
     explicit DamagesDialog(QWidget *parent = nullptr);
+    static QStringList data;
     ~DamagesDialog();
+
+private slots:
+    void on_addButton_clicked();
+    void on_editButton_clicked();
+    void on_deleteButton_clicked();
 
 private:
     Ui::DamagesDialog *ui;
+    QStringListModel *model;
 };
 
 #endif // DAMAGESDIALOG_H
